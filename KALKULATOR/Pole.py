9d15fe5b535e74cc207cbd33ea1ed62283e8d490
root <root@localhost.localdomain>
2017-07-15 16:25:41 +0000

    Pierwszy commit i pierwszy projekt

---
+++ KALKULATOR/Pole.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import math
+import time
+time.sleep(0.4)
+number = input("Podaj dlugosc boku a\n")
+liczba_number = number * number  
+number2 = math.sqrt(9)  
+number3 = 4
+time.sleep(0.4)
+print("Pole trojkata rownobocznego wynosi: " )
+print(liczba_number)
+print("Pierwiastek z: " + str (number2))
+print("Na " + str (number3))
+
---
+++ KALKULATOR/pierwiastek.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+import math
+number = input("Podaj liczbe do spierwiastkowania\n")
+float_number = float(number)
+print ("Pierwiastek liczby to: " + str(math.sqrt(float_number)))
