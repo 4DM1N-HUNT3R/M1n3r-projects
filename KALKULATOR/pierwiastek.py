5280934f4e479009a22f71a9f3f16731682e1833
4DM1N-HUNT3R <midzon262@gmail.com>
2017-09-14 18:51:35 +0000

    Update pierwiastek.py

--- KALKULATOR/pierwiastek.py
+++ KALKULATOR/pierwiastek.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 
 import math
-number = input("Podaj liczbe do spierwiastkowania\n")
+import sys
+
+number = raw_input("Podaj liczbe do spierwiastkowania\n")
 float_number = float(number)
+if len(number) != 0: 
+	print '\033[93m' "Wpisales zero lub nic nie wpisales"
+	sys.exit()
+
 print ("Pierwiastek liczby to: " + str(math.sqrt(float_number)))
